9864f7cd594517ab7804e0a2dc20832e3aff4d89
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2024-04-29 13:13:54 -0400

    Create an Interop module for working with ScalaJS and the @finos/mock-morphir-elm module

---
+++ app/scalajs-cli/src/morphir/elm/interop/MockMorphirElm.scala
@@ -0,0 +1,9 @@
+package morphir.elm.interop
+import scala.scalajs.js 
+import js.annotation.* 
+
+@js.native
+@JSImport("@finos/mock-morphir-elm", JSImport.Namespace)
+object MockMorphirElm extends  js.Object {
+  
+}
