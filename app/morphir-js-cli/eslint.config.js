a5bacf760086e8d1a56c7359735c542124f46a32
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2024-04-30 09:24:39 -0400

    eslint config related changes

--- app/morphir-js-cli/eslint.config.js
+++ app/morphir-js-cli/eslint.config.js
@@ -2,3 +2,31 @@ import pluginJs from "@eslint/js";
 import parser from "@typescript-eslint/parser";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+
+export default [
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+      languageOptions: {
+        parser,
+        parserOptions: {
+          ecmaVersion: 2020,
+          sourceType: "module",
+        },
+      },
+      // todo add "plugins": ["import", "simple-import-sort"],
+      rules: {
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          { argsIgnorePattern: "^_" },
+        ],
+        // "import/first": "error",
+        // "import/no-cycle": "error",
+        // "import/newline-after-import": "error",
+        // "import/no-duplicates": "error",
+        "@typescript-eslint/consistent-type-imports": "error",
+      },
+    },
+  ];
